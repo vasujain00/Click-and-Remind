acea6498aa8a2c30df00b2fbd142c28cb1a2f380
vasujain00 <www.vasujain@gmail.com>
2016-11-14 11:29:43 +0530

    Initial
    ocr with training data working on several fonts.  camera intent running

---
+++ app/src/main/java/com/example/vasu/ocr/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.vasu.ocr;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Uri fileUri;
+    // Activity request codes
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    private static final int CAMERA_CAPTURE_VIDEO_REQUEST_CODE = 200;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    Bitmap image; //our image
+    ImageView imageView;
+    private TessBaseAPI mTess; //Tess API reference
+    String datapath =""; //path to folder containing language data file
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         imageView = (ImageView) findViewById(R.id.imageView);
+        //init image
+        image=((BitmapDrawable)imageView.getDrawable()).getBitmap();
+        //image = BitmapFactory.decodeResource(getResources(), R.drawable.test_image2);
+
+        datapath = getFilesDir()+ "/tesseract/";
+
+        //make sure training data has been copied
+        checkFile(new File(datapath + "tessdata/"));
+
+        //init Tesseract API
+        String language = "eng";
+
+        mTess = new TessBaseAPI();
+        mTess.init(datapath, language);
+    }
+
+
+    private void copyFile() {
+        try {
+            //location we want the file to be at
+            String filepath = datapath + "/tessdata/eng.traineddata";
+
+            //get access to AssetManager
+            AssetManager assetManager = getAssets();
+
+            //open byte streams for reading/writing
+            InputStream instream = assetManager.open("tessdata/eng.traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            //copy the file to the location specified by filepath
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkFile(File dir) {
+        //directory does not exist, but we can successfully create it
+        if (!dir.exists()&& dir.mkdirs()){
+            copyFile();
+        }
+        //The directory exists, but there is no data file in it
+        if(dir.exists()) {
+            String datafilepath = datapath+ "/tessdata/eng.traineddata";
+            File datafile = new File(datafilepath);
+            if (!datafile.exists()) {
+                copyFile();
+            }
+        }
+    }
+
+    public void processImage(View view){
+
+        image=((BitmapDrawable)imageView.getDrawable()).getBitmap();
+        mTess.setImage(image);
+        String OCRresult = mTess.getUTF8Text();
+        TextView OCRTextView = (TextView) findViewById(R.id.OCRTextView);
+        OCRTextView.setText(OCRresult);
+    }
+
+    public void clickImage(View view)
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
